5468b1834b239187ce92153cccf23eaa96face3d
Silvio Melo <114765722+SilvioCMJ@users.noreply.github.com>
2023-03-29 13:16:31 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import openai
+import SpeechRecognition as sr
+#key individual da openai
+openai.api_key = "sk-..."
+#puxando audio
+filename = "exemplo.mp3"
+r = sr.Recognizer()
+
+with sr.AudioFile(filename) as source:
+    #escutando o audio
+    audio_data = r.record(source)
+    #convertendo em texto
+    text = r.recognize_google(audio_data)
+    #transformando o txt em img
+    response = openai.Image.create(prompt=text, n=1, size='1024x1024')
+
+    img_url = response['data'][0]['url']
+    print(f'URL da imagem gerada: {img_url}')
+
